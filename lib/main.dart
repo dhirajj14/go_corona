0848c005a314e1eae78a1eafd4295a0e5451aefb
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-04-01 20:12:58 -0500

    add fonts and heading

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 import 'custom_icons.dart' as CustomIcons;
+import 'package:intl/intl.dart';
 
 void main() => runApp(TotalWidget());
 
@@ -67,25 +68,37 @@ class _TotalWidgetState extends State<TotalWidget> {
             backgroundColor: Colors.red[800],
           ),
 
-          body: Container(
-                        child: RefreshIndicator(
-                          key: new GlobalKey<RefreshIndicatorState>(),
-                          onRefresh: () async {
-                            setTotal();
-                          },
-                          child: GridView.count(
-                            shrinkWrap: true,
-                            crossAxisCount: 2,
-                            padding: EdgeInsets.all(3.0),
-                            children: <Widget>[
-                              makeDashboardItem(data[0]['confirmed'], Icons.book),
-                              makeDashboardItem(data[0]['recovered'], Icons.alarm),
-                              makeDashboardItem(data[0]['critical'], Icons.alarm),
-                              makeDashboardItem(data[0]['deaths'], Icons.alarm),
-                            ],
-                          ),
+          body:
+              Column(
+                children: <Widget>[
+                 new Container(
+                    child: new Padding(padding: EdgeInsets.all(10.0),
+                    child: new Text("World Data",
+                        style: new TextStyle(fontSize: 35.0,fontWeight: FontWeight.bold, color: Colors.red[800]),
+                      ),
+                    ),
+                  ),
+                  new Container(
+                                child: RefreshIndicator(
+                                  key: new GlobalKey<RefreshIndicatorState>(),
+                                  onRefresh: () async {
+                                    setTotal();
+                                  },
+                                  child: GridView.count(
+                                    shrinkWrap: true,
+                                    crossAxisCount: 2,
+                                    padding: EdgeInsets.all(3.0),
+                                    children: <Widget>[
+                                      makeDashboardItem(data[0]['confirmed'], "Confirmed"),
+                                      makeDashboardItem(data[0]['recovered'], "Recovered"),
+                                      makeDashboardItem(data[0]['critical'], "Critical"),
+                                      makeDashboardItem(data[0]['deaths'], "Deaths"),
+                                    ],
+                                  ),
+                      ),
+          ),
+                ],
               ),
-            ),
           bottomNavigationBar: BottomNavigationBar(
             items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
@@ -135,12 +148,12 @@ class APIService {
   }
 }
 
-Card makeDashboardItem(String title, IconData icon) {
+Card makeDashboardItem(String title, String name) {
   return Card(
       elevation: 1.0,
       margin: new EdgeInsets.all(8.0),
       child: Container(
-        decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+        decoration: BoxDecoration(color: Colors.red[800], borderRadius: BorderRadius.all(Radius.circular(5.0))),
         child: new InkWell(
           onTap: () {},
           child: Column(
@@ -150,16 +163,19 @@ Card makeDashboardItem(String title, IconData icon) {
             children: <Widget>[
               SizedBox(height: 50.0),
               Center(
-                  child: Icon(
-                    icon,
-                    size: 40.0,
-                    color: Colors.black,
-                  )),
+                child: new Text(name, style: TextStyle(fontSize: 20.0, color: Colors.white,),),
+//                  child: Icon(
+//                    icon,
+//                    size: 40.0,
+//                    color: Colors.black,
+//                  )
+              ),
               SizedBox(height: 20.0),
               new Center(
-                child: new Text(title,
+                child: new Text(NumberFormat("#,##,###", "en_US").format(int.parse(title)),
                     style:
-                    new TextStyle(fontSize: 18.0, color: Colors.black)),
+                    new TextStyle(fontSize: 22.0, color: Colors.white, fontFamily: 'Orbitron', fontWeight: FontWeight.w900,letterSpacing: 2.0),
+                ),
               )
             ],
           ),
